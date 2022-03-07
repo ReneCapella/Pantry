03c1d66bad11c57f61f3bfb11290654017ecb9d5
renecapella <renecapella.dev@gmail.com>
2022-03-07 09:33:08 -0800

    PANTRY-12 making mistakes and fixing them: forgot to delete prevous migrations

--- db/migrate/20220305175706_add_main_to_usepantriess.rb
+++
@@ -1,5 +0,0 @@
-class AddMainToUsepantriess < ActiveRecord::Migration[7.0]
-  def change
-    add_column :user_pantries, :primary, :boolean, default: false, null: false
-  end
-end
--- db/migrate/20220307171650_add_columns.rb
+++ db/migrate/20220307171650_add_columns.rb
@@ -1,6 +1,6 @@
 class AddColumns < ActiveRecord::Migration[7.0]
   def change
     add_column :pantries, :primary, :boolean, default: false, null: false
-    add_column :pantries, :pantry_name, :string, default: "my pantry"
+    add_column :pantries, :name, :string, default: "my pantry"
   end
 end
