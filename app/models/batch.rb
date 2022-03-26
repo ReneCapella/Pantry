bb9c6b53a37914e61da39345f58f18f0ad899849
renecapella <renecapella.dev@gmail.com>
2022-03-25 18:26:08 -0700

    PANTRY-5 recalled items have name updates after batch is marked at recalled

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -1,4 +1,16 @@
 class Batch < ApplicationRecord
   belongs_to :producer
   has_many :food_items
+
+  after_update :food_recall
+
+  def food_recall
+    if self.recall
+
+      self.food_items.each do |food_item|
+        old_name = food_item.name
+        food_item.update!(name: '!!RECALLED!! ' + old_name)
+      end
+    end
+  end
 end
